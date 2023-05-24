5337526461da0ec751f97ec098278d46efcc2eff
Marat-Shainurov <m_shainurov@mail.ru>
2023-05-24 14:37:35 +0300

    view and index update

--- main/views.py
+++ main/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from main.models import MainChannels
+
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = list(MainChannels.objects.all().values())
+    context = {'channels': data}
+    return render(request, 'main/index.html', context)
