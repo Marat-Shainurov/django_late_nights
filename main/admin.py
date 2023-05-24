e46cad777264fc1ed1be80c41e217957fce716fc
Marat-Shainurov <m_shainurov@mail.ru>
2023-05-24 14:04:24 +0300

    models and admin.py update

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models import MainChannels, MainVideos
+
+
+@admin.register(MainChannels)
+class ChannelsAdmin(admin.ModelAdmin):
+    list_display = ('channel_id', 'title', 'views', 'subscribers', 'videos', 'channel_url')
+    list_filter = ('title', 'channel_id')
+
+
+@admin.register(MainVideos)
+class VideosAdmin(admin.ModelAdmin):
+    list_display = ('channel', 'title', 'video_id', 'publish_date', 'video_url')
+    list_filter = ('publish_date',)
+    search_fields = ('title',)
--- main/models.py
+++ main/models.py
@@ -1,28 +1,31 @@
 from django.db import models
 
-
-class Channels(models.Model):
-    channel_id = models.IntegerField(primary_key=True, verbose_name='channel_id')
-    title = models.CharField(max_length=100, verbose_name='channel_title')
-    views = models.IntegerField(verbose_name='channel_views')
-    subscribers = models.IntegerField(verbose_name='channel_subscribers')
-    videos_number = models.IntegerField(verbose_name='videos_number')
-    channel_url = models.URLField(verbose_name='channel_url')
+class MainChannels(models.Model):
+    channel_id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=250)
+    views = models.IntegerField()
+    subscribers = models.IntegerField()
+    videos = models.IntegerField()
+    channel_url = models.CharField(max_length=255)
 
     def __str__(self):
-        return f'{self.channel_id} {self.title} {self.channel_url}'
+        return f'{self.title}'
 
     class Meta:
-        verbose_name = 'channel'
-        verbose_name_plural = 'channels'
+        managed = False
+        db_table = 'channels'
 
 
-class Videos(models.Model):
-    video_id = models.IntegerField(verbose_name='video_id')
-    channel = models.ForeignKey(Channels, on_delete=models.CASCADE, to_field='channel_id', verbose_name='channel_id')
+class MainVideos(models.Model):
+    video_id = models.BigAutoField(primary_key=True)
     title = models.TextField(verbose_name='video_title')
-    publish_date = models.DateTimeField(verbose_name='publish_date')
-    video_url = models.IntegerField(verbose_name='video_url')
+    publish_date = models.DateTimeField()
+    video_url = models.CharField(max_length=255)
+    channel = models.ForeignKey(MainChannels, models.DO_NOTHING, to_field='channel_id')
 
     def __str__(self):
         return '{0} {1} {2}'.format(self.video_id, self.title, self.video_url)
+
+    class Meta:
+        managed = False
+        db_table = 'videos'
