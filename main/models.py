8360509312f7e356515440cb10ba7dcaea3ca9ce
Marat-Shainurov <m_shainurov@mail.ru>
2023-05-24 12:27:48 +0300

    models've been added and migrated

--- main/models.py
+++ main/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Channels(models.Model):
+    channel_id = models.IntegerField(primary_key=True, verbose_name='channel_id')
+    title = models.CharField(max_length=100, verbose_name='channel_title')
+    views = models.IntegerField(verbose_name='channel_views')
+    subscribers = models.IntegerField(verbose_name='channel_subscribers')
+    videos_number = models.IntegerField(verbose_name='videos_number')
+    channel_url = models.URLField(verbose_name='channel_url')
+
+    def __str__(self):
+        return f'{self.channel_id} {self.title} {self.channel_url}'
+
+    class Meta:
+        verbose_name = 'channel'
+        verbose_name_plural = 'channels'
+
+
+class Videos(models.Model):
+    video_id = models.IntegerField(verbose_name='video_id')
+    channel = models.ForeignKey(Channels, on_delete=models.CASCADE, to_field='channel_id', verbose_name='channel_id')
+    title = models.TextField(verbose_name='video_title')
+    publish_date = models.DateTimeField(verbose_name='publish_date')
+    video_url = models.IntegerField(verbose_name='video_url')
+
+    def __str__(self):
+        return '{0} {1} {2}'.format(self.video_id, self.title, self.video_url)
